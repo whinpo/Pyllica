1551cdcec0f567aeacaf0b49160aa2b5201ddd7d
whinpo <whinpo@yahoo.fr>
2019-05-25 08:38:01 +0200

    d/l par item direct

--- bulletin1878-1935.py
+++ bulletin1878-1935.py
@@ -10,11 +10,11 @@ from pathlib import Path
 from requests_html import HTMLSession
 import certifi
 
-#anneedebut=1878
-#anneefin=1936
-anneedebut=1933
-anneefin=1933
+anneedebut=1878
+anneefin=1936
+
 for annee in range(anneedebut, anneefin):
+    print(annee)
     download_dir="/home/whinpo/Philatélie/Daguins/Infos/Bulletins mensuels des postes/{0}".format(annee)
 
     # si le répertoire n'existe pas on le crée
@@ -39,70 +39,71 @@ for annee in range(anneedebut, anneefin):
 
     #page_html.html.html contient le html avec javascript exécuté
     soup=bs.BeautifulSoup(page_html.html.html,'lxml')
+    # on récupère tous les liens de l'année
+    resultat=soup.find_all('span',{"class": "day-number"})
+    for spans in resultat:
+        if spans.a.get('href'):
+            finalurl=spans.a.get('href')
+            # finalurl="https://gallica.bnf.fr/ark:/12148/cb32730626t/date{0}0101".format(annee)
+            print(finalurl)
 
-    # on regarde si la page a affiché une liste de documents ou si elle affiche directement un item
-    # exemple : https://gallica.bnf.fr/ark:/12148/cb32730626t/date19330111 =>
-    # donne en fait une url https://gallica.bnf.fr/ark:/12148/bpt6k5827116v.item
-    if ".item" in urldest:
-        # on enlève item et on met pdf à la place
-        lienPDF="{0}{1}".format(urldest.split("item")[0],"pdf")
-        lienTXT="{0}{1}".format(urldest.split("item")[0],"txt")
-        # title de la forme [<title>Bulletin mensuel des postes et télégraphes | 1933-01-11 | Gallica</title>]
-        titre=soup.find('title').text.split(" | ")[1]
-        fichierPDF="{0}/PDF/{1}pdf".format(download_dir,titre)
-        fichierTXT="{0}/TXT/{1}txt".format(download_dir,titre)
+            #urllib.request.urlopen(finalurl)
+            #urllib.request.urlopen(finalurl)
+            session = HTMLSession()
+            page_html=session.get(finalurl)
 
-        if not os.path.exists(fichierPDF):
-            print("Téléchargement de {0} vers {1}".format(lienPDF,fichierPDF))
-            wget.download(lienPDF,fichierPDF)
-        else:
-            print("{0} déjà existant".format(fichierPDF))
-        if not os.path.exists(fichierTXT):
-            print("Téléchargement de {0} vers {1}".format(lienTXT,fichierTXT))
-            wget.download(lienTXT,fichierTXT)
-        else:
-            print("{0} déjà existant".format(fichierTXT))
-    else:
+            # on exécute le javascript
+            print('rendering suite {0}...'.format(finalurl))
+            page_html.html.render(sleep=2)
 
-            # on récupère tous les liens de l'année
-            resultat=soup.find_all('span',{"class": "day-number"})
-            for spans in resultat:
-                if spans.a.get('href'):
-                    finalurl=spans.a.get('href')
-                    # finalurl="https://gallica.bnf.fr/ark:/12148/cb32730626t/date{0}0101".format(annee)
-                    print(finalurl)
+            #page_html.html.html contient le html avec javascript exécuté
+            soup=bs.BeautifulSoup(page_html.html.html,'lxml')
 
+            # on regarde si la page a affiché une liste de documents ou si elle affiche directement un item
+            # exemple : https://gallica.bnf.fr/ark:/12148/cb32730626t/date19330111 =>
+            # donne en fait une url https://gallica.bnf.fr/ark:/12148/bpt6k5827116v.item
+            urldest=page_html.url
+            print("urldest {0}".format(urldest))
+            if ".item" in urldest:
+                print("lien direct {0}".format(urldest))
+                # on enlève item et on met pdf à la place
+                lienPDF="{0}{1}".format(urldest.split("item")[0],"pdf")
+                lienTXT="{0}{1}".format(urldest.split("item")[0],".texteBrut")
+                # title de la forme [<title>Bulletin mensuel des postes et télégraphes | 1933-01-11 | Gallica</title>]
+                titre=soup.find('title').text.split(" | ")[1]
+                fichierPDF="{0}/PDF/{1}.pdf".format(download_dir,titre)
+                fichierTXT="{0}/TXT/{1}.txt".format(download_dir,titre)
 
-                    #urllib.request.urlopen(finalurl)
-                    #urllib.request.urlopen(finalurl)
-                    page_html=session.get(finalurl)
+                if not os.path.exists(fichierPDF):
+                    print("Téléchargement de {0} vers {1}".format(lienPDF,fichierPDF))
+                    wget.download(lienPDF,fichierPDF)
+                else:
+                    print("{0} déjà existant".format(fichierPDF))
+                if not os.path.exists(fichierTXT):
+                    print("Téléchargement de {0} vers {1}".format(lienTXT,fichierTXT))
+                    wget.download(lienTXT,fichierTXT)
+                else:
+                    print("{0} déjà existant".format(fichierTXT))
+            else:
+                # on extrait les résultats de la recherche
+                for numeros in soup.find_all('div',class_='resultat_img'):
+                    # lien vers url en enlevent ce qu'il ya derrière le ? pour le remplacer par .pdf
+                    lienPDF="{0}{1}".format(numeros.a.get('href').split('?')[0],'.pdf')
+                    lienTXT="{0}{1}".format(numeros.a.get('href').split('?')[0],'.texteBrut')
+                    titre=numeros.a.span.get('title').replace('/','-')
+                    fichierPDF="{0}/PDF/{1}pdf".format(download_dir,titre)
+                    fichierTXT="{0}/TXT/{1}txt".format(download_dir,titre)
 
-                    # on exécute le javascript
-                    print('rendering {0}...'.format(finalurl))
-                    page_html.html.render(sleep=2)
-
-                    #page_html.html.html contient le html avec javascript exécuté
-                    soup=bs.BeautifulSoup(page_html.html.html,'lxml')
-
-                    # on extrait les résultats de la recherche
-                    for numeros in soup.find_all('div',class_='resultat_img'):
-                        # lien vers url en enlevent ce qu'il ya derrière le ? pour le remplacer par .pdf
-                        lienPDF="{0}{1}".format(numeros.a.get('href').split('?')[0],'.pdf')
-                        lienTXT="{0}{1}".format(numeros.a.get('href').split('?')[0],'.texteBrut')
-                        titre=numeros.a.span.get('title').replace('/','-')
-                        fichierPDF="{0}/PDF/{1}pdf".format(download_dir,titre)
-                        fichierTXT="{0}/TXT/{1}txt".format(download_dir,titre)
-
-                        if not os.path.exists(fichierPDF):
-                            print("Téléchargement de {0} vers {1}".format(lienPDF,fichierPDF))
-                            wget.download(lienPDF,fichierPDF)
-                        else:
-                            print("{0} déjà existant".format(fichierPDF))
-                        if not os.path.exists(fichierTXT):
-                            print("Téléchargement de {0} vers {1}".format(lienTXT,fichierTXT))
-                            wget.download(lienTXT,fichierTXT)
-                        else:
-                            print("{0} déjà existant".format(fichierTXT))
+                    if not os.path.exists(fichierPDF):
+                        print("Téléchargement de {0} vers {1}".format(lienPDF,fichierPDF))
+                        wget.download(lienPDF,fichierPDF)
+                    else:
+                        print("{0} déjà existant".format(fichierPDF))
+                    if not os.path.exists(fichierTXT):
+                        print("Téléchargement de {0} vers {1}".format(lienTXT,fichierTXT))
+                        wget.download(lienTXT,fichierTXT)
+                    else:
+                        print("{0} déjà existant".format(fichierTXT))
 # on execute le javascript
 # r.html.render()
 # soup=BeautifulSoup(r.text,"lxml")
